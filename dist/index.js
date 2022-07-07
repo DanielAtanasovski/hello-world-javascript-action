5ff66a3f0e4f33739acaf07c1036335bfd9a008f
Daniel Atanasovski <daniel.atanasovski@reece.com.au>
2022-07-07 12:47:40 +1000

    print array changes

--- dist/index.js
+++ dist/index.js
@@ -9014,7 +9014,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  const commits = github.context.payload["commits"]
+  const commits = JSON.stringify(github.context.payload["commits"], undefined, 2)
   console.log(`Commits array: ${commits}`)
 } catch (error) {
   core.setFailed(error.message);
